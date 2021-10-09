b6038c0ea27abfe0893463a46e466173664cb637
Prashantsetia <66926991+Prashantsetia@users.noreply.github.com>
2021-10-09 09:54:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var vid1 = document.getElementById("vid1");
+var over = document.getElementById("over");
+var over2 = document.getElementById("over2");
+vid1.onpause = () => {
+  if (vid1.getAttribute("src") === "clip1.mp4") {
+    console.log("FUCK");
+    vid1.style.filter = "brightness(20%)";
+    over.style.zIndex = 1;
+  } else {
+    console.log("FUCK else");
+    vid1.style.filter = "brightness(20%)";
+    over2.style.zIndex = 1;
+  }
+};
+
+function fun2() {
+  vid1.setAttribute("src", "clip2.mp4");
+  over.style.zIndex = 0;
+  vid1.style.filter = "brightness(100%)";
+  vid1.play();
+}
+
+function fun3() {
+  vid1.setAttribute("src", "clip3.mp4");
+  over2.style.zIndex = 0;
+  vid1.style.filter = "brightness(100%)";
+  vid1.play();
+}
