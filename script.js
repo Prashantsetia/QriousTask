4a6f4a4e8e1286d29c6f4511a6277d02bb1bd745
Prashantsetia <66926991+Prashantsetia@users.noreply.github.com>
2021-10-10 06:23:53 +0530

    Modified button's event handling

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
-var vid1 = document.getElementById("vid1");
+var vidplayer = document.getElementById("vidplayer");
 var over = document.getElementById("over");
 var over2 = document.getElementById("over2");
-vid1.onpause = () => {
-  if (vid1.getAttribute("src") === "clip1.mp4") {
-    console.log("one");
-    vid1.style.filter = "brightness(20%)";
-    over.style.zIndex = 1;
+var head = document.getElementById("head");
+
+vidplayer.onpause = () => {
+  if (vidplayer.getAttribute("src") === "clip1.mp4") {
+    console.log("Overlay for Clip 1");
+    vidplayer.style.filter = "brightness(20%)";
+    over.style.zIndex = 1;  // hiding the video by frame 1 --> options Home and Photography
   } else {
-    console.log("two");
-    vid1.style.filter = "brightness(20%)";
-    over2.style.zIndex = 1;
+    console.log("Overlay for clip 2");
+    vidplayer.style.filter = "brightness(20%)";
+    over2.style.zIndex = 1; // hiding the video by frame 2 if clip1 is not running (means - it is for clip 2) --> options Home and Photography
   }
 };
 
 function fun2() {
-  vid1.setAttribute("src", "clip3.mp4");
+  vidplayer.setAttribute("src", "clip2.mp4");
   over.style.zIndex = 0;
-  vid1.style.filter = "brightness(100%)";
-  vid1.play();
+  vidplayer.style.filter = "brightness(100%)";
+  vidplayer.play();
 }
 
 function fun3() {
-  vid1.setAttribute("src", "clip2.mp4");
+  vidplayer.setAttribute("src", "clip3.mp4");
   over2.style.zIndex = 0;
-  vid1.style.filter = "brightness(100%)";
-  vid1.play();
+  vidplayer.style.filter = "brightness(100%)";
+  vidplayer.play();
+}
+
+
+extra = ()=>{
+  head.innerHTML = "This Option Is Disabled For Now !!";
+  head.style.color = "red";
+
+  setTimeout(() => {
+    head.innerHTML = "Pause The Video To See The Magic";
+    head.style.color = "black";
+  }, 3000);
 }
